8b969e15944b943db97eb3a06cbe498edd6caa9e
Nikolas Novo <nikolasnovo03@gmail.com>
2022-04-05 15:02:11 -0300

    Documentação + Criação da Função que cria uma playlist

--- linkedlist.c
+++ linkedlist.c
@@ -2,3 +2,16 @@
 
 #include "linkedlist.h"
 
+/**
+* @brief Função que aloca um espaço do tamanho de uma Playlist na memória
+* @return novaPlaylist, a nova Playlist com os apontamentos de memória para o início, fim e atual apontando para NULL
+*/
+Playlist* llcriaPlaylist() {
+	Playlist* novaPlaylist = (Playlist*)malloc(sizeof(Playlist));
+
+	novaPlaylist->inicio = NULL;
+	novaPlaylist->atual = NULL;
+	novaPlaylist->fim = NULL;
+
+	return novaPlaylist;
+}
--- linkedlist.h
+++ linkedlist.h
@@ -3,12 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Definição das palavras-chave, cada uma vai ter uma string e um apontamento para uma próxima, caso existir
 typedef struct palavrasChave
 {
 	char categoria[64];
 	struct palavrasChave* outraCategoria;
 } PalavrasChave;
 
+//Definição do meu podcast, vai possuir um id, uma string nome(podcast), uma string nome(episódio) e um numero do episódio
+//TODO: implementar as palavras-chave
 typedef struct podcast
 {
 	int podcastId;
@@ -18,11 +21,19 @@ typedef struct podcast
 
 } Podcast;
 
+//Definindo que um ponteiro para um Podcast vai se chamar Podquest
 typedef Podcast* Podquest;
 
+//Definição da minha lista, possuindo 3 ponteiros: início, atual e fim
 typedef struct playlist
 {
 	Podquest inicio;
 	Podquest atual;
 	Podquest fim;
 } Playlist;
+
+/**
+* @brief Função que aloca um espaço do tamanho de uma Playlist na memória
+* @return novaPlaylist, a nova Playlist com os apontamentos de memória para o início, fim e atual apontando para NULL
+*/
+Playlist* llcriaPlaylist();
