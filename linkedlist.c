d4fff96c83670b901857e95c4e9ab55282e67c70
Nikolas Novo <nikolasnovo03@gmail.com>
2022-04-08 14:40:09 -0300

    Protótipos para criação de palavras chave e podquest adicionados

--- linkedlist.c
+++ linkedlist.c
@@ -15,3 +15,19 @@ Playlist* llcriaPlaylist() {
 
 	return novaPlaylist;
 }
+
+/**
+* @brief Função que aloca um espaço do tamanho de uma PalavrasChave na memória (dados dessa PalavrasChave serão pedidos nessa função)
+* @return novaPalavrasChave, a nova PalavrasChave com os apontamentos de memória para o próximo apontando para NULL
+*/
+PalavrasChave* criaPalavrasChave() {
+
+}
+
+/**
+* @brief Função que aloca um espaço do tamanho de um Podquest na memória (dados desse Podquest serão pedidos nessa função)
+* @return novoPodquest, a nova Playlist com os apontamentos de memória para o próximo e anterior apontando para NULL
+*/
+Podquest criaPodquest() {
+
+}
--- linkedlist.h
+++ linkedlist.h
@@ -2,12 +2,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //Definição das palavras-chave, cada uma vai ter uma string e um apontamento para uma próxima, caso existir
 typedef struct palavrasChave
 {
 	char categoria[64];
-	struct palavrasChave* outraCategoria;
+	struct palavrasChave* proxima;
+
 } PalavrasChave;
 
 //Definição do meu podcast, vai possuir um id, uma string nome(podcast), uma string nome(episódio) e um numero do episódio
@@ -18,6 +20,11 @@ typedef struct podcast
 	char nomePodcast[64];
 	char nomeEpisodio[64];
 	int numeroEpisodio;
+
+	PalavrasChave palavrasChave;
+
+	struct podcast* proximo;
+	struct podcast* anterior;
 
 } Podcast;
 
@@ -30,6 +37,7 @@ typedef struct playlist
 	Podquest inicio;
 	Podquest atual;
 	Podquest fim;
+	
 } Playlist;
 
 /**
@@ -37,3 +45,15 @@ typedef struct playlist
 * @return novaPlaylist, a nova Playlist com os apontamentos de memória para o início, fim e atual apontando para NULL
 */
 Playlist* llcriaPlaylist();
+
+/**
+* @brief Função que aloca um espaço do tamanho de uma PalavrasChave na memória (dados dessa PalavrasChave serão pedidos nessa função)
+* @return novaPalavrasChave, a nova PalavrasChave com os apontamentos de memória para o próximo apontando para NULL
+*/
+PalavrasChave* criaPalavrasChave();
+
+/**
+* @brief Função que aloca um espaço do tamanho de um Podquest na memória (dados desse Podquest serão pedidos nessa função)
+* @return novoPodquest, a nova Playlist com os apontamentos de memória para o próximo e anterior apontando para NULL
+*/
+Podquest criaPodquest();
