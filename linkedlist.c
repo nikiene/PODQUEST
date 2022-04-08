9432b7d0403b2cbad990485ffcc41fdeb59b8dea
Nikolas Novo <nikolasnovo03@gmail.com>
2022-04-08 15:16:54 -0300

    Começando ã implementar as funções

--- linkedlist.c
+++ linkedlist.c
@@ -2,11 +2,21 @@
 
 #include "linkedlist.h"
 
+//Função auxiliar que troca '\n' por '\0' (thx @Yarquen! :D)
+void remove_newline_ch(char* line) {
+	int new_line = strlen(line) - 1;
+	if (line[new_line] == '\n')
+	{
+		line[new_line] = '\0';
+	}
+}
+
 /**
 * @brief Função que aloca um espaço do tamanho de uma Playlist na memória
 * @return novaPlaylist, a nova Playlist com os apontamentos de memória para o início, fim e atual apontando para NULL
 */
 Playlist* llcriaPlaylist() {
+
 	Playlist* novaPlaylist = (Playlist*)malloc(sizeof(Playlist));
 
 	novaPlaylist->inicio = NULL;
@@ -16,12 +26,36 @@ Playlist* llcriaPlaylist() {
 	return novaPlaylist;
 }
 
+
 /**
-* @brief Função que aloca um espaço do tamanho de uma PalavrasChave na memória (dados dessa PalavrasChave serão pedidos nessa função)
-* @return novaPalavrasChave, a nova PalavrasChave com os apontamentos de memória para o próximo apontando para NULL
+* @brief Função que aloca um espaço do tamanho de uma lista de PalavraChave na memória 
+* @return novaPalavrasChave, a nova lista de PalavraChave com os apontamentos de memória para o início e fim apontando para NULL
 */
 PalavrasChave* criaPalavrasChave() {
 
+	PalavrasChave* novaPalavrasChave = (PalavrasChave*)malloc(sizeof(PalavrasChave));
+
+	novaPalavrasChave->inicio = NULL;
+	novaPalavrasChave->fim = NULL;
+
+	return novaPalavrasChave;
+}
+
+/**
+* @brief Função que aloca um espaço do tamanho de uma PalavraChave na memória (dados dessa PalavraChave serão pedidos nessa função)
+* @return novaPalavrasChave, a nova PalavraChave com os apontamentos de memória para o próximo apontando para NULL
+*/
+PalavraChave* criaPalavraChave() {
+	
+	PalavraChave* novaPalavraChave = (PalavraChave*)malloc(sizeof(PalavraChave));
+
+	printf("\nPalavra-chave: ");
+	fgets(novaPalavraChave->categoria, 64, stdin);
+	remove_newline_ch(novaPalavraChave->categoria);
+
+	novaPalavraChave->proxima = NULL;
+
+	return novaPalavraChave;
 }
 
 /**
--- linkedlist.h
+++ linkedlist.h
@@ -5,15 +5,22 @@
 #include <string.h>
 
 //Definição das palavras-chave, cada uma vai ter uma string e um apontamento para uma próxima, caso existir
-typedef struct palavrasChave
+typedef struct palavraChave
 {
 	char categoria[64];
-	struct palavrasChave* proxima;
+	struct palavraChave* proxima;
+
+} PalavraChave;
+
+//Definindo que uma Lista de Palavras-chave.
+typedef struct palavrasChave {
+
+	PalavraChave* inicio;
+	PalavraChave* fim;
 
 } PalavrasChave;
 
 //Definição do meu podcast, vai possuir um id, uma string nome(podcast), uma string nome(episódio) e um numero do episódio
-//TODO: implementar as palavras-chave
 typedef struct podcast
 {
 	int podcastId;
@@ -47,10 +54,16 @@ typedef struct playlist
 Playlist* llcriaPlaylist();
 
 /**
-* @brief Função que aloca um espaço do tamanho de uma PalavrasChave na memória (dados dessa PalavrasChave serão pedidos nessa função)
-* @return novaPalavrasChave, a nova PalavrasChave com os apontamentos de memória para o próximo apontando para NULL
+* @brief Função que aloca um espaço do tamanho de uma lista de PalavraChave na memória
+* @return novaPalavrasChave, a nova lista de PalavraChave com os apontamentos de memória para o início e fim apontando para NULL
 */
 PalavrasChave* criaPalavrasChave();
+
+/**
+* @brief Função que aloca um espaço do tamanho de uma PalavraChave na memória (dados dessa PalavraChave serão pedidos nessa função)
+* @return novaPalavrasChave, a nova PalavraChave com os apontamentos de memória para o próximo apontando para NULL
+*/
+PalavraChave* criaPalavraChave();
 
 /**
 * @brief Função que aloca um espaço do tamanho de um Podquest na memória (dados desse Podquest serão pedidos nessa função)
