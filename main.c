2e8e6de9f2b5c33e23f136c09822ceaf080870d1
Nikolas Novo <nikolasnovo03@gmail.com>
2022-04-10 14:43:13 -0300

    inicio do menu

--- main.c
+++ main.c
@@ -7,8 +7,27 @@
 int main(int argc, char const* argv[]) 
 {
 	Playlists* listaPlaylists = criaPlaylists();
+	Playlist* playlistAtual;
 
-	
+	printf("                                                                                \n");
+	printf("                            ,,                                                  \n");
+	printf("                          `7MM                                            mm    \n");
+	printf("                            MM                                            MM    \n");
+	printf("`7MMpdMAo.  ,pW\"Wq.    ,M\"\"bMM    ,dW\"Yvd  `7MM  `7MM   .gP\"Ya, pP\"Ybd  mmMMmm  \n");
+	printf("  MM   `Wb 6W'   `Wb ,AP    MM   ,W'   MM    MM    MM  ,M'   Yb 8I   `\"   MM\n");
+	printf("  MM    M8 8M     M8 8MI    MM   8M    MM    MM    MM  8M\"\"\"\"\"\" `YMMMa.   MM    \n");
+	printf("  MM   ,AP YA.   ,A9 `Mb    MM   YA.   MM    MM    MM  YM.    , L.   I8   MM    \n");
+	printf("  MMbmmd'   `Ybmd9'   `Wbmd\"MML.  `MbmdMM    `Mbod\"YML. `Mbmmd' M9mmmP'   `Mbmo \n");
+	printf("  MM                                   MM                                       \n");
+	printf(".JMML.                               .JMML.                     version 1.0.0 :p\n");
+
+	int option = 0;
+	bool running = true;
+
+	while (running)
+	{
+
+	}
 
 	return 0;
 }
