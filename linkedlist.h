105383abde94c9cd0537e102ce74f7f0c361e4fc
Nikolas Kiene <80795579+nikiene@users.noreply.github.com>
2022-04-10 23:41:23 -0300

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -1,172 +1,182 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 #include <time.h>
 
-//Função auxiliar que troca '\n' por '\0' (thx @Yarquen! :D)
-void remove_newline_ch(char* line);
+// FunÃ§Ã£o auxiliar que troca '\n' por '\0' (thx @Yarquen! :D)
+void remove_newline_ch(char *line);
 
-//Definição das palavras-chave, cada uma vai ter uma string e um apontamento para uma próxima, caso existir
-typedef struct palavraChave
-{
-	char categoria[64];
-	struct palavraChave* proxima;
-	struct palavraChave* anterior;
+// DefiniÃ§Ã£o das palavras-chave, cada uma vai ter uma string e um apontamento
+// para uma prÃ³xima, caso existir
+typedef struct palavraChave {
+  char categoria[64];
+  struct palavraChave *proxima;
+  struct palavraChave *anterior;
 
 } PalavraChave;
 
-//Definindo que uma Lista de Palavras-chave.
+// Definindo que uma Lista de Palavras-chave.
 typedef struct palavrasChave {
-
-	PalavraChave* inicio;
-	PalavraChave* fim;
+  PalavraChave *inicio;
+  PalavraChave *fim;
 
 } PalavrasChave;
 
-//Definição do meu podcast, vai possuir um id, uma string nome(podcast), uma string nome(episódio) e um numero do episódio
-typedef struct podcast
-{
-	int podcastId;
-	char nomePodcast[64];
-	char nomeEpisodio[64];
-	int numeroEpisodio;
+// DefiniÃ§Ã£o do meu podcast, vai possuir um id, uma string nome(podcast), uma
+// string nome(episÃ³dio) e um numero do episÃ³dio
+typedef struct podcast {
+  int podcastId;
+  char nomePodcast[64];
+  char nomeEpisodio[64];
+  int numeroEpisodio;
 
-	PalavrasChave* palavrasChave;
+  PalavrasChave *palavrasChave;
 
-	struct podcast* proximo;
-	struct podcast* anterior;
+  struct podcast *proximo;
+  struct podcast *anterior;
 
 } Podcast;
 
-//Definindo que um ponteiro para um Podcast vai se chamar Podquest
-typedef Podcast* Podquest;
+// Definindo que um ponteiro para um Podcast vai se chamar Podquest
+typedef Podcast *Podquest;
 
-//Definição da minha lista, possuindo 3 ponteiros: início, atual e fim
-typedef struct playlist
-{
-	char nomePlaylist[64];
+// DefiniÃ§Ã£o da minha lista, possuindo 3 ponteiros: inÃ­cio, atual e fim
+typedef struct playlist {
+  char nomePlaylist[64];
 
-	Podquest inicio;
-	Podquest atual;
-	Podquest fim;
+  Podquest inicio;
+  Podquest atual;
+  Podquest fim;
 
-	struct playlist* proxima;
-	
+  struct playlist *proxima;
+
 } Playlist;
 
-//Lista de Playlists
-typedef struct playlists
-{
-	Playlist* inicio;
-	Playlist* fim;
+// Lista de Playlists
+typedef struct playlists {
+  Playlist *inicio;
+  Playlist *fim;
 
-}Playlists;
-
-/**
-* @brief Função que aloca um espaço do tamanho de uma Playlist na memória
-* @return novaPlaylist, a nova Playlist com os apontamentos de memória para o início, fim e atual apontando para NULL
-*/
-Playlist* criaPlaylist();
+} Playlists;
 
 /**
-* @brief Função que aloca um espaço do tamanho de uma Lista de Playlist na memória
-* @return novasPlaylists, a nova Lista de Playlist com os apontamentos de memória para o início e próxima apontando para NULL
-*/
-Playlists* criaPlaylists();
+ * @brief FunÃ§Ã£o que aloca um espaÃ§o do tamanho de uma Playlist na memÃ³ria
+ * @return novaPlaylist, a nova Playlist com os apontamentos de memÃ³ria para o
+ * inÃ­cio, fim e atual apontando para NULL
+ */
+Playlist *criaPlaylist();
 
 /**
-* @brief Função que mostra as Playlists na lista de Playlists
-* @param playlists -> lista de Playlists
-*/
-void llmostraPlaylists(Playlists* playlists);
+ * @brief FunÃ§Ã£o que aloca um espaÃ§o do tamanho de uma Lista de Playlist na
+ * memÃ³ria
+ * @return novasPlaylists, a nova Lista de Playlist com os apontamentos de
+ * memÃ³ria para o inÃ­cio e prÃ³xima apontando para NULL
+ */
+Playlists *criaPlaylists();
 
 /**
-* @brief Função que mostra as Playlists na lista de Playlists e pede ao usuário selecionar uma
-* @param playlists -> lista de Playlists
-* @return aux -> Playlist selecionada
-*/
-Playlist* llselecionaPlaylist(Playlists* playlists);
+ * @brief FunÃ§Ã£o que mostra as Playlists na lista de Playlists
+ * @param playlists -> lista de Playlists
+ */
+void llmostraPlaylists(Playlists *playlists);
 
 /**
-* @brief Função que adiciona uma Playlist na lista de Playlists
-* @param playlists -> lista de Playlists
-*/
-void lladicionaNovaPlaylist(Playlists* playlists);
+ * @brief FunÃ§Ã£o que mostra as Playlists na lista de Playlists e pede ao usuÃ¡rio
+ * selecionar uma
+ * @param playlists -> lista de Playlists
+ * @return aux -> Playlist selecionada
+ */
+Playlist *llselecionaPlaylist(Playlists *playlists);
 
 /**
-* @brief Função que aloca um espaço do tamanho de uma lista de PalavraChave na memória
-* @return novaPalavrasChave, a nova lista de PalavraChave com os apontamentos de memória para o início e fim apontando para NULL
-*/
-PalavrasChave* criaPalavrasChave();
+ * @brief FunÃ§Ã£o que adiciona uma Playlist na lista de Playlists
+ * @param playlists -> lista de Playlists
+ */
+void lladicionaNovaPlaylist(Playlists *playlists);
 
 /**
-* @brief Função que aloca um espaço do tamanho de uma PalavraChave na memória (dados dessa PalavraChave serão pedidos nessa função)
-* @return novaPalavrasChave, a nova PalavraChave com os apontamentos de memória para o próximo apontando para NULL
-*/
-PalavraChave* criaPalavraChave();
+ * @brief FunÃ§Ã£o que aloca um espaÃ§o do tamanho de uma lista de PalavraChave na
+ * memÃ³ria
+ * @return novaPalavrasChave, a nova lista de PalavraChave com os apontamentos
+ * de memÃ³ria para o inÃ­cio e fim apontando para NULL
+ */
+PalavrasChave *criaPalavrasChave();
 
 /**
-* @brief adiciona uma palavra-chave no fim da lista de palavras-chave
-* @param palavrasChave -> Lista de palavras-chave
-* @return 1 -> se mais palavras vão ser inseridas
-* @return 0 -> se mais palavras vão ser inseridas
-*/
-int adicionaPalavraChave(PalavrasChave* palavrasChave);
+ * @brief FunÃ§Ã£o que aloca um espaÃ§o do tamanho de uma PalavraChave na memÃ³ria
+ * (dados dessa PalavraChave serÃ£o pedidos nessa funÃ§Ã£o)
+ * @return novaPalavrasChave, a nova PalavraChave com os apontamentos de memÃ³ria
+ * para o prÃ³ximo apontando para NULL
+ */
+PalavraChave *criaPalavraChave();
 
 /**
-* @brief Função que aloca um espaço do tamanho de um Podquest na memória (dados desse Podquest serão pedidos nessa função)
-* @return novoPodquest, a nova Playlist com os apontamentos de memória para o próximo e anterior apontando para NULL
-*/
+ * @brief adiciona uma palavra-chave no fim da lista de palavras-chave
+ * @param palavrasChave -> Lista de palavras-chave
+ * @return 1 -> se mais palavras vÃ£o ser inseridas
+ * @return 0 -> se mais palavras vÃ£o ser inseridas
+ */
+int adicionaPalavraChave(PalavrasChave *palavrasChave);
+
+/**
+ * @brief FunÃ§Ã£o que aloca um espaÃ§o do tamanho de um Podquest na memÃ³ria (dados
+ * desse Podquest serÃ£o pedidos nessa funÃ§Ã£o)
+ * @return novoPodquest, a nova Playlist com os apontamentos de memÃ³ria para o
+ * prÃ³ximo e anterior apontando para NULL
+ */
 Podquest criaPodquest();
 
 /**
-* @brief adiciona um Podquest no final da Playlist
-* @param playlist -> Lista de Podquest
-*/
-void llInserePodquestFim(Playlist* playlist);
+ * @brief adiciona um Podquest no final da Playlist
+ * @param playlist -> Lista de Podquest
+ */
+void llInserePodquestFim(Playlist *playlist);
 
 /**
-* @brief Função que verifica se um Podquest existe na Playlist, e caso exista, o retorna
-* @param playlist -> Playlist na qual vai acontecer a busca
-* @param idBusca -> Id do Podquest que vai ser buscado
-* @param epsBusca -> Episodio do Podquest que vai ser buscado
-* @return aux -> O Podquest buscaso
-* @return NULL -> Caso o Podquest não exista na lista
-*/
-Podquest llBuscaPodquest(Playlist* playlist, int idBusca, int epsBusca);
+ * @brief FunÃ§Ã£o que verifica se um Podquest existe na Playlist, e caso exista,
+ * o retorna
+ * @param playlist -> Playlist na qual vai acontecer a busca
+ * @param idBusca -> Id do Podquest que vai ser buscado
+ * @param epsBusca -> Episodio do Podquest que vai ser buscado
+ * @return aux -> O Podquest buscaso
+ * @return NULL -> Caso o Podquest nÃ£o exista na lista
+ */
+Podquest llBuscaPodquest(Playlist *playlist, int idBusca, int epsBusca);
 
 /**
-* @brief Função que busca um elemento na lista e se o encontrar, o remove
-* @param playlist -> Playlist a qual vai ter o Podquest removido
-* @param idBusca -> Id do Podquest que vai ser buscado
-* @param epsBusca -> Episodio do Podquest que vai ser buscado
-*/
-void llremovePodquest(Playlist* playlist, int idBusca, int epsBusca);
+ * @brief FunÃ§Ã£o que busca um elemento na lista e se o encontrar, o remove
+ * @param playlist -> Playlist a qual vai ter o Podquest removido
+ * @param idBusca -> Id do Podquest que vai ser buscado
+ * @param epsBusca -> Episodio do Podquest que vai ser buscado
+ */
+void llremovePodquest(Playlist *playlist, int idBusca, int epsBusca);
 
 /**
-* @brief Função que começa a tocar a playlist
-* @param playlist -> Playlist que vai ser tocada
-*/
-void lltocar(Playlist* playlist);
+ * @brief FunÃ§Ã£o que comeÃ§a a tocar a playlist
+ * @param playlist -> Playlist que vai ser tocada
+ */
+void lltocar(Playlist *playlist);
 
 /**
-* @brief Função que toca o próximo Podquest da PLaylist, caso o shuffle esteja ligado, toca um dos próximos Podquests aleatoriamente
-* @param playlist -> Playlist que vai ser tocada
-* @param shuffle -> Boolean dizendo se está ligado ou não
-*/
-void lltocaProximo(Playlist* playlist, bool shuffle);
+ * @brief FunÃ§Ã£o que toca o prÃ³ximo Podquest da PLaylist, caso o shuffle esteja
+ * ligado, toca um dos prÃ³ximos Podquests aleatoriamente
+ * @param playlist -> Playlist que vai ser tocada
+ * @param shuffle -> Boolean dizendo se estÃ¡ ligado ou nÃ£o
+ */
+void lltocaProximo(Playlist *playlist, bool shuffle);
 
 /**
-* @brief Função que toca o próximo Podquest da PLaylist, caso o shuffle esteja ligado, toca um dos próximos Podquests aleatoriamente
-* @param playlist -> Playlist que vai ser tocada
-*/
-void lltocaAnterior(Playlist* playlist);
+ * @brief FunÃ§Ã£o que toca o prÃ³ximo Podquest da PLaylist, caso o shuffle esteja
+ * ligado, toca um dos prÃ³ximos Podquests aleatoriamente
+ * @param playlist -> Playlist que vai ser tocada
+ */
+void lltocaAnterior(Playlist *playlist);
 
 /**
-* @brief Função que imprime os Podquests da Playlist
-* @param playlist -> playlist que vai ser impressa
-*/
-void llrelatorio(Playlist* playlist);
+ * @brief FunÃ§Ã£o que imprime os Podquests da Playlist
+ * @param playlist -> playlist que vai ser impressa
+ */
+void llrelatorio(Playlist *playlist);
